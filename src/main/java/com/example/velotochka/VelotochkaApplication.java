7c27eb57a0e8c9fb7c3ce708031db6ed60410f57
Mikita Shunko <mikita.shunko@gmail.com>
2022-06-17 19:22:46 +0200

    Setup project

---
+++ src/main/java/com/example/velotochka/VelotochkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.velotochka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelotochkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelotochkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/velotochka/VelotochkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.velotochka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelotochkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
